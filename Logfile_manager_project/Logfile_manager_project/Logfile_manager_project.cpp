26aa7aa1c2832fbaa88e6668c8837d0e3cefde52
tkdahs26 <48806275+tkdahs26@users.noreply.github.com>
2026-01-30 14:22:00 +0900

    Update Logfile_manager_project.cpp

--- Logfile_manager_project/Logfile_manager_project/Logfile_manager_project.cpp
+++ Logfile_manager_project/Logfile_manager_project/Logfile_manager_project.cpp
@@ -1,4 +1,4 @@
-﻿#include <iomanip>
+#include <iomanip>
 #include <chrono>
 #include <ctime>
 #include <filesystem>
@@ -26,7 +26,7 @@ public:
     void delete_File(const std::string& filename);
     void rename_File(const std::string& oldName, const std::string& newName);
 private:
-    std::map<std::string, std::unique_ptr<std::fstream>> logFiles;
+    std::map<std::string, std::unique_ptr<std::fstream>> logFiles;// 스마트포인터활용 fstream생성
 
 };
 
@@ -38,21 +38,21 @@ void LogFileManager::make_or_open_File(const std::string& filename) {
     }
 }
 
-void LogFileManager::write_File(const std::string &filename, const std::string& message) {
-    auto it = logFiles.find(filename); 
+void LogFileManager::write_File(const std::string& filename, const std::string& message) {
+    auto it = logFiles.find(filename);
 
     if (it != logFiles.end()) {
         auto now = std::chrono::system_clock::now();
         std::time_t now_c = std::chrono::system_clock::to_time_t(now);
         struct tm timeinfo;
-        localtime_s(&timeinfo, &now_c);
-      
+        localtime_s(&timeinfo, &now_c);// localtime_s 함수 내부의 역참조를 위해 주소 연산자로 호출
+
         *(it->second) << "[" << std::put_time(&timeinfo, "%Y-%m-%d %H:%M:%S") << "] " << message << std::endl;
-    }
+    }  // *( (*it).second)   사용자정의 operator*()호출
 }
 
 std::vector<std::string> LogFileManager::readLogs(const std::string& filename) {
-    std::vector<std::string> logs;  //vector[] = 자바의 arraylist[]
+    std::vector<std::string> logs;  //vector[] = 자바의 arraylist와 비슷
     std::ifstream file(filename); // ifstream생성자호출 filename열기
 
     if (file.is_open()) {
@@ -72,10 +72,10 @@ void LogFileManager::close_File(const std::string& filename) {
 }
 
 void LogFileManager::delete_File(const std::string& filename) {
-    //먼저 메모리에서 제거해 파일을 닫음
-    logFiles.erase(filename);
 
-   
+    logFiles.erase(filename);  //먼저 메모리에서 제거해 파일을 닫음
+
+
     if (std::filesystem::remove(filename)) {
         std::cout << "파일이 삭제되었습니다: " << filename << std::endl;
     }
@@ -84,16 +84,16 @@ void LogFileManager::delete_File(const std::string& filename) {
     }
 }
 
- 
+
 void LogFileManager::rename_File(const std::string& oldName, const std::string& newName) {
-    // 메모리에 해당 파일이 있는지 확인
-    auto it = logFiles.find(oldName);
+
+    auto it = logFiles.find(oldName);   // 메모리에 해당 파일이 있는지 확인
     if (it == logFiles.end()) {
         std::cout << "변경할 파일이 목록에 없습니다." << std::endl;
         return;
     }
-    //현재 열려있는 파일을 메모리에서 제거해야 변경가능
-    logFiles.erase(it);
+
+    logFiles.erase(it);//현재 열려있는 파일을 메모리에서 제거해야 변경가능
 
     //파일 이름 변경
     std::error_code error_code;
